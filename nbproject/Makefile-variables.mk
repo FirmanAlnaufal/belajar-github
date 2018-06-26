7aafdd485434d8aedb0e2fd3cda11eb01d874add
FimanAlnaufal <alnaufal96@gmail.com>
2018-06-26 16:59:45 +0700

    Ini adalah commit pertama

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=Cygwin-Windows
+CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin-Windows
+CND_ARTIFACT_NAME_Debug=projekgrafis
+CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin-Windows/projekgrafis
+CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin-Windows/package
+CND_PACKAGE_NAME_Debug=projekgrafis.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin-Windows/package/projekgrafis.tar
+# Release configuration
+CND_PLATFORM_Release=Cygwin-Windows
+CND_ARTIFACT_DIR_Release=dist/Release/Cygwin-Windows
+CND_ARTIFACT_NAME_Release=projekgrafis
+CND_ARTIFACT_PATH_Release=dist/Release/Cygwin-Windows/projekgrafis
+CND_PACKAGE_DIR_Release=dist/Release/Cygwin-Windows/package
+CND_PACKAGE_NAME_Release=projekgrafis.tar
+CND_PACKAGE_PATH_Release=dist/Release/Cygwin-Windows/package/projekgrafis.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
